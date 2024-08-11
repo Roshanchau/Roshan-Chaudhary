8a2e06564887cd14acefdf00cbe06ebe304bdbac
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-08-11 22:34:35 +0545

    feat: footer suru hehe

---
+++ app/components/footer.tsx
@@ -0,0 +1,13 @@
+import Container from "./ui/container"
+
+const Footer= ()=>{
+    return (
+        <Container>
+            <div className=" px-4 flex items-center mx-6">
+                foot.
+            </div>
+        </Container>
+    )
+}
+
+export default Footer
--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,8 +5,18 @@ const Hero = () => {
         <h2 className="font-normal text-xl text-[rgb(95,241,208)] mb-4">
           Hi, my name is
         </h2>
-        <h1 className="text-[60px]">Roshan Chaudhary.</h1>
+        <h1 className="text-[60px] mb-3 text-[rgb(204,214,246)]">Roshan Chaudhary.</h1>
+        <h1 className="text-[55px] text-[rgb(136,146,176)]">
+          Aspiring Software Developer.
+        </h1>
+
       </div>
+        {/* description */}
+        <p className="mt-6 w-[500px] text-[rgb(118,129,158)] text-base font-medium">
+          I'm a software engineer specializing in building (and occasionally
+          designing) exceptional digital experiences. Currently, I'm focused on
+          building accessible, human-centered products.
+        </p>
     </div>
   );
 };
