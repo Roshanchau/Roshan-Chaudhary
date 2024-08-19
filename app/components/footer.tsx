1c04271efc24e05fca9b58139dc3714fd87fd194
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-08-19 23:08:46 +0545

    footer icons  added.

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,14 @@
 import Container from "./ui/container"
+import { FaGithub , FaLinkedin , FaTwitterSquare , FaInstagram } from "react-icons/fa";
 
 const Footer= ()=>{
     return (
         <Container>
-            <div className=" px-4 flex items-center mx-6">
-                foot.
+            <div className=" px-4 flex flex-row items-center mx-6 gap-6">
+                <FaGithub size={20}/>
+                <FaLinkedin size={20}/>
+                <FaTwitterSquare size={20}/>
+                <FaInstagram size={20}/>
             </div>
         </Container>
     )
