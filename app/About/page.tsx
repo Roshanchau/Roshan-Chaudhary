2996d3ed944b4df2a8d237401457ed3819f6c11d
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-08-27 21:50:34 +0545

    okay

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -9,13 +9,13 @@ const About = () => {
       {/* info */}
       <div className="flex flex-col items-center  w-[600px] text-[rgb(204,214,246)] p-10">
         <span>
-        Hello! My name is Roshan Chaudhary and I am currently learning software
+        Hello! My name is Roshan Chaudhary and I am currently learning software 
         development and how things work around in the world of computer. I am
         proficient in frontend as well as backend. My proficiency is javascript.</span><br/>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text everI will try fix you. 
         since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
+        scrambled it to make a type specimen book. It has survived not only five jejej
         centuries, but also the leap into electronic typesetting, remaining
         essentially unchanged. It was popularised in the 1960s with the release 
         of Letraset sheets containing Lorem Ipsum passages, and more recently
