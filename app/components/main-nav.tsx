b086a9a7deb40c4d3e2a69ee19fd80acd6950c50
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-10-05 22:02:52 +0545

    blogs and logo done!!

--- app/components/main-nav.tsx
+++ app/components/main-nav.tsx
@@ -1,67 +1,71 @@
-"use client"
+"use client";
 
-import {  useEffect, useState } from "react";
-import {cn} from "../lib/utils";
+import { useEffect, useState } from "react";
+import { cn } from "../lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { SunMoon } from 'lucide-react';
+import { SunMoon } from "lucide-react";
 import { useThemeStore } from "../store/useThemeStore";
 
-
-interface MainNavProps{
-    label: string[];
+interface MainNavProps {
+  label: string[];
 }
 
-const MainNav: React.FC<MainNavProps>=({
-    label
-})=>{
-    const { theme , toggleTheme } = useThemeStore();
+const MainNav: React.FC<MainNavProps> = ({ label }) => {
+  const { theme, toggleTheme } = useThemeStore();
 
-    const pathname= usePathname()
+  const pathname = usePathname();
 
-    const routes= label.map((route)=>({
-        label: route
-    }))
+  const routes = label.map((route) => ({
+    label: route,
+  }));
 
-    useEffect(()=>{
-        if(theme === 'light'){
-        document.documentElement.classList.add('white-bg');
-        }else{
-        document.documentElement.classList.remove('white-bg');
-        }
-    },[theme])
+  useEffect(() => {
+    if (theme === "light") {
+      document.documentElement.classList.add("white-bg");
+    } else {
+      document.documentElement.classList.remove("white-bg");
+    }
+  }, [theme]);
 
-    const handleToggle = () => {
-        console.log('toggle clicked');
-        // setToggle(!toggle);
-        if(theme === 'light'){
-        document.documentElement.classList.add('white-bg');
-        }else{
-        document.documentElement.classList.remove('white-bg');
-        }
-        toggleTheme();
-    };
+  const handleToggle = () => {
+    console.log("toggle clicked");
+    // setToggle(!toggle);
+    if (theme === "light") {
+      document.documentElement.classList.add("white-bg");
+    } else {
+      document.documentElement.classList.remove("white-bg");
+    }
+    toggleTheme();
+  };
 
-    return (
-        <nav
-        className="mx-8 flex items-center space-x-4 lg:space-x-12"
-    >
-        {
-            routes.map((route)=>(
-                <Link 
-                key={route.label}
-                href={`/${route.label}`}
-                    className={cn(
-                        "text-base font-medium  transition-colors hover:text-neutral-400 cursor-pointer"
-                    )}
-                >
-                    {route.label}
-                </Link>
-            ))
-        }
-        <SunMoon onClick={handleToggle}  className={`cursor-pointer transition-colors hover:text-neutral-400 `}/>
+  return (
+    <nav className="mx-8 flex items-center space-x-4 lg:space-x-12">
+      {routes.map((route) => (
+        <Link
+          key={route.label}
+          href={`/${route.label}`}
+          className={cn(
+            "text-base font-medium  transition-colors hover:text-neutral-400 cursor-pointer"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+      <Link
+        href="https://roshanblogs.hashnode.dev/"
+        className={cn(
+          "text-base font-medium  transition-colors hover:text-neutral-400 cursor-pointer"
+        )}
+      >
+        Blogs
+      </Link>
+      <SunMoon
+        onClick={handleToggle}
+        className={`cursor-pointer transition-colors hover:text-neutral-400 `}
+      />
     </nav>
-    )
-}
+  );
+};
 
 export default MainNav;
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,6 +2,8 @@
 import Link from "next/link";
 import Container from "../components/ui/container";
 import MainNav from "../components/main-nav";
+import { TbHexagonLetterRFilled } from "react-icons/tb";
+
 
 export const revalidate=0;
 
@@ -11,9 +13,9 @@ const Navbar = async () => {
       <Container>
         <div className=" px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">ROSHAN</p>
+            <p className="font-bold text-xl"><TbHexagonLetterRFilled size={40}/></p>
           </Link>
-          <MainNav label={ ['About' , 'Projects' , 'Blogs' ]} />
+          <MainNav label={ ['About' , 'Projects'  ]} />
         </div>
       </Container>
     </div>
